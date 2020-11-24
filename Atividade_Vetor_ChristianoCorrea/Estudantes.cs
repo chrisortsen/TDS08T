a305c09808e31ae5249cfe11a5e604c0c017bfff
chrisortsen <christiano.correa98@gmail.com>
2020-11-24 19:55:51 -0300

    Atividade 19 Fixacao Vetores

---
+++ Atividade_Vetor_ChristianoCorrea/Estudantes.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula13_VetoresPT2
+{
+    class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        // construtor
+        public Estudantes(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+    }
+}
---
+++ Atividade_Vetor_ChristianoCorrea/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula13_VetoresPT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            // sintaxe do vetor
+            // tipo[] nomeDoVetor = new tipo[tamanho]
+            Estudantes[] estudante = new Estudantes[n];
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Insira o nome do estudante: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o email do estudante: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite o numero do quarto desejado: ");
+                int quarto = int.Parse(Console.ReadLine());
+                estudante[i] = new Estudantes(nome, email, quarto);
+                Console.WriteLine();
+            }
+            Console.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i+1) +": ");
+                Console.WriteLine("Nome: " + estudante[i].Nome);
+                Console.WriteLine("Email: " + estudante[i].Email);
+                Console.WriteLine("Quarto: " + estudante[i].Quarto);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(estudante[i].Quarto + ": " + estudante[i].Nome + ", " + estudante[i].Email);
+            }
+        }
+    }
+}
